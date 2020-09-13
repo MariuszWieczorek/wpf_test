c5bf31bfda25a2a2a2c185363181cfa56aa4e41b
Mariusz Wieczorek <m.wieczorek1972@gmail.com>
2020-09-13 20:59:52 +0200

    Add project files.

---
+++ CustomCommands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace wpf_test
+{
+    // gdy chcemy utworzyć własne komendy o innych nazwach niż predefiniowane: New, Cut, Paste itd
+    // musimu utworzyć własną klasę statyczną jak poniżej
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand
+            (
+                "Exit",
+                "Exit",
+                typeof(CustomCommands),
+                new InputGestureCollection()
+                {
+                    new KeyGesture(Key.F10, ModifierKeys.Alt)
+                }
+            );
+
+        //Define more commands here, just like the one above
+    }
+}
---
+++ Ekrany/WiazanieDanych01.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpf_test
+{
+    /// <summary>
+    /// Interaction logic for WiazanieDanych01.xaml
+    /// </summary>
+    public partial class WiazanieDanych01 : Window
+    {
+        public WiazanieDanych01()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        // commands
+        private void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("The New command was invoked");
+        }
+
+        private void ExitCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("The Exit command was invoked");
+            Application.Current.Shutdown();
+
+        }
+    }
+}
